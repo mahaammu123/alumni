4327d063b98cd6041901192e04904f38f961db29
mahaammu123 <31887486+mahaammu123@users.noreply.github.com>
2017-09-12 16:53:56 +0530

    Add files via upload

---
+++ 2.f
@@ -0,0 +1,69 @@
+<html>
+<head>
+<script type="text/javascript" src="validate.js"></script>
+</head>
+<body>
+<form action="#" name="signup page" onsubmit="return(validate());">
+
+<table cellpadding="5" width="50%" bgcolor="white" align="center"
+cellspacing="8">
+
+<tr>
+<td colspan=2>
+<center><font size=4><b>CREATE AN ACCOUNT</b></font></center>
+</td>
+</tr>
+
+<tr>
+<td>Name</td>
+<td><input type=text name=textnames id="textname" size="30"></td>
+</tr>
+<tr>
+<td>Last Name</td>
+<td><input type="text" name="lastname" id="lastname" size="30"></td>
+</tr>
+<tr>
+<td>Age</td>
+<td><input type="text" name="age" id="age" size="30"></td>
+</tr>
+<tr>
+<td>Sex</td>
+<td><input type="radio" name="sex" value="male" size="10">Male
+<input type="radio" name="sex" value="Female" size="10">Female</td>
+</tr>
+<tr>
+<td>DOB</td>
+<td>
+<input type="date" name="dob"  size="30"></td>
+</tr>
+<tr>
+<td>Department</td>
+<td><input type="text" name="department" id="department" size="30"></td>
+</tr>
+<tr>
+<td>Email ID</td>
+<td><input type="text" name="Email ID" id="Email ID" size="30"></td>
+</tr>
+<tr>
+<td>MobileNo</td>
+<td><input type="text" name="mobileno" id="mobileno" size="30"></td>
+</tr>
+<tr>
+<td>Password</td>
+<td><input type="password" name="password" id="password" size="30"></td>
+</tr>
+<tr>
+<td>Retype password</td>
+<td><input type="password" name="retype password" id="retype password" size="30"></td>
+</tr>
+<tr>
+<td>Username or ID</td>
+<td><input type="text" name="text" id="text" size="30"></td>
+</tr>
+<tr>
+<td colspan="2"><center><button>CREATE AN ACCOUNT</center></button></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
---
+++ 5.f
@@ -0,0 +1,146 @@
+<html>
+<head>
+<script type="text/javascript" src="validate.js"></script>
+</head>
+<body>
+<form action="#" name="AlumniRegistration" onsubmit="return(validate());">
+
+<table cellpadding="2" width="20%" bgcolor="00FFFF" align="center"
+cellspacing="2">
+
+<tr>
+<td colspan=2>
+<center><font size=4><b>Alumni Registration Form</b></font></center>
+</td>
+</tr>
+
+<tr>
+<td>Title</td>
+<td><input type=text name=textTitle id="textTitle" size="30"></td>
+</tr>
+
+<tr>
+<td>First Name</td>
+<td><input type="text" name="firstname" id="firstname"
+size="30"></td>
+</tr>
+<tr>
+<td>Prefred name</td>
+<td><input type="text" name="prefred name" id="prefred name" size="30"></td>
+</tr>
+
+<tr>
+<td>family name</td>
+<td><input type="text" name="family name"
+id="family name" size="30"></td>
+</tr>
+
+<tr>
+<td>Name at time of graduation</td>
+<td><input type="text" name="Name at time of graduation"
+id="family name" size="30"></td>
+</tr>
+
+<tr>
+<td>Date of birth</td>
+<td><input type="text" name="Dtae of birth"
+id="Date bof birth" size="30"></td>
+</tr>
+
+<tr>
+<td colspan=2>
+<center><font size=4><b>Student details</b></font></center>
+</td>
+</tr>
+
+<tr>
+<td>Student number</td>
+<td><input type="text" name="Student number"
+id="Student number" size="30"></td>
+</td>
+
+<tr>
+<td>Graduation year</td>
+<td><input type="text" name="Graduation year"
+id="Graduation year" size="30"></td>
+</td>
+
+<tr>
+<td>Degree completed</td>
+<td><input type="text" name="Degree completed"
+id="Degree completed" size="30"></td>
+</td>
+
+<tr>
+<td colspan=2>
+<center><font size=4><b>Professional details </b></font></center>
+</td>
+</tr>
+
+<tr>
+<td>Organization</td>
+<td><input type="text" name="Organization"
+id="Organization" size="30"></td>
+</td>
+
+<tr>
+<td>Business direct time</td>
+<td><input type="text" name="Business direct time"
+id="business direct time" size="30"></td>
+</td>
+
+<tr>
+<td>Business email</td>
+<td><input type="text" name="Business email"
+id="Business email" size="30"></td>
+</td>
+
+<tr>
+<td>Sex</td>
+<td><input type="radio" name="sex" value="male" size="10">Male
+<input type="radio" name="sex" value="Female" size="10">Female</td>
+</tr>
+
+<tr>
+<td>City</td>
+<td><select name="City">
+<option value="-1" selected>select..</option>
+<option value="New Delhi">NEW DELHI</option>
+<option value="Mumbai">MUMBAI</option>
+<option value="Goa">GOA</option>
+<option value="Patna">PATNA</option>
+</select></td>
+</tr>
+
+<tr>
+<td>PinCode</td>
+<td><input type="text" name="pincode" id="pincode" size="30"></td>
+
+</tr>
+<tr>
+<td>EmailId</td>
+<td><input type="text" name="emailid" id="emailid" size="30"></td>
+</tr>
+
+<tr>
+<td>DOB</td>
+<td><input type="text" name="dob" id="dob" size="30"></td>
+</tr>
+
+<tr>
+<td>NEWS</td>
+<td><input type="text" name="tamil" id="english" size="30"></td>
+</tr>
+
+<tr>
+<td>MobileNo</td>
+<td><input type="text" name="mobileno" id="mobileno" size="30"></td>
+</tr>
+<tr>
+<td><input type="reset"></td>
+<td colspan="2"><input type="submit" value="Submit Form" /></td>
+</tr>
+</table>
+</form>
+</body>
+</html>
